59c252b826e1b2c0f20c762767bdeed3eaf3991c
Theo <theo@noreply.org>
2023-06-08 23:49:14 -0700

    Eslint fix

--- components/Notices.vue
+++ components/Notices.vue
@@ -18,12 +18,7 @@
         </template>
         <template v-slot:description>{{ notice.description }}</template>
       </i18n>
-      <a
-        v-if="notice.nova"
-        href="https://nova.tornado.ws"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <a v-if="notice.nova" href="https://nova.tornado.ws/" target="_blank" rel="noopener noreferrer">
         Tornado Cash Nova
       </a>
       <a
