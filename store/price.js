7db929cc8c3a694d2742fef1278925f86b7fdc74
Theo <theo@noreply.org>
2023-08-21 10:22:17 -0700

    Make token price api faster & easier via lib @tornado/tornado-oracles

--- store/price.js
+++ store/price.js
@@ -1,58 +1,30 @@
 /* eslint-disable no-console */
-import { toBN, toChecksumAddress } from 'web3-utils'
-
-import networkConfig from '@/networkConfig'
-import offchainOracleABI from '@/abis/OffchainOracle.abi.json'
-
-const offchainOracleAddress = '0x07D91f5fb9Bf7798734C3f606dB065549F6893bb'
-
-const TOKENS = {
-  torn: {
-    tokenAddress: '0x77777FeDdddFfC19Ff86DB637967013e6C6A116C',
-    symbol: 'TORN',
-    decimals: 18
-  }
-}
+import { TokenPriceOracle } from '@tornado/tornado-oracles'
 
 export const state = () => {
   return {
     prices: {
-      dai: '593970928097706',
-      cdai: '12623454013395',
-      usdc: '593319851383838',
-      usdt: '592852719537467',
-      torn: '85362951428474830',
-      wbtc: '32269526951862905063'
+      torn: '1607657994944809',
+      dai: '604623353553108',
+      cdai: '13524059864784',
+      usdc: '600920027684692',
+      usdt: '600764414690498',
+      wbtc: '15636492243151834302'
     }
   }
 }
 
 export const getters = {
-  tokenRate: (state, getters, rootState, rootGetters) => {
+  priceOracle: (state, getters, rootState, rootGetters) => {
+    const netId = Number(rootGetters['metamask/netId'])
+    const { url: rpcUrl } = rootState.settings[`netId${netId}`].rpc
+
+    return new TokenPriceOracle(rpcUrl)
+  },
+  tokenRate: (state, getters, rootState) => {
     return state.prices[rootState.application.selectedStatistic.currency]
   },
-  getArgsForOracle: (state, getters, rootState, rootGetters) => {
-    const tokens = {
-      ...networkConfig.netId1.tokens,
-      ...TOKENS
-    }
-    const tokenAddresses = []
-    const oneUintAmount = []
-    const currencyLookup = {}
-    Object.entries(tokens).map(([currency, data]) => {
-      if (currency !== 'eth') {
-        tokenAddresses.push(data.tokenAddress)
-        oneUintAmount.push(
-          toBN('10')
-            .pow(toBN(data.decimals.toString()))
-            .toString()
-        )
-        currencyLookup[data.tokenAddress] = currency
-      }
-    })
-    return { tokenAddresses, oneUintAmount, currencyLookup }
-  },
-  getTokenPrice: (state, getters, rootState, rootGetters) => (currency) => {
+  getTokenPrice: (state) => (currency) => {
     return state.prices[currency]
   },
   isPriceWatcherDisabled: (state, getters, rootState, rootGetters) => {
@@ -78,30 +50,7 @@ export const actions = {
     }
 
     try {
-      const web3 = this.$provider.getWeb3(rootState.settings.netId1.rpc.url)
-      const offchainOracle = new web3.eth.Contract(offchainOracleABI, offchainOracleAddress)
-      const { tokenAddresses, oneUintAmount, currencyLookup } = getters.getArgsForOracle
-
-      const prices = {}
-      for (let i = 0; i < tokenAddresses.length; i++) {
-        try {
-          const isWrap =
-            toChecksumAddress(tokenAddresses[i]) ===
-            toChecksumAddress('0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643')
-
-          const price = await offchainOracle.methods.getRateToEth(tokenAddresses[i], isWrap).call()
-          const numerator = toBN(oneUintAmount[i])
-          const denominator = toBN(10).pow(toBN(18)) // eth decimals
-          const priceFormatted = toBN(price)
-            .mul(numerator)
-            .div(denominator)
-
-          prices[currencyLookup[tokenAddresses[i]]] = priceFormatted.toString()
-        } catch (e) {
-          console.error('cant get price of ', tokenAddresses[i])
-        }
-      }
-
+      const prices = await getters.priceOracle.fetchPrices()
       console.log('prices', prices)
       commit('SAVE_TOKEN_PRICES', prices)
 
